2564d14f930cb9031ede2893ba56fa838c9098f2
CherniakYura <juracherniak@gmail.com>
2023-09-04 22:49:23 +0300

    Add support for negative rect sizes to olivec_fill_rect

--- olive.c
+++ olive.c
@@ -20,6 +20,8 @@ typedef int Errno;
     a = b;                                                                     \
     b = t;                                                                     \
   } while (0)
+#define OLIVEC_SIGN(T, x) ((T)((x) > 0) - (T)((x) < 0))
+#define OLIVEC_ABS(T, x) (OLIVEC_SIGN(T, x) * (x))
 
 void olivec_fill(uint32_t *pixels, size_t width, size_t height,
                  uint32_t color) {
@@ -62,13 +64,18 @@ defer:
 }
 
 void olivec_fill_rect(uint32_t *pixels, size_t pixels_width,
-                      size_t pixels_height, int x0, int y0, size_t w, size_t h,
+                      size_t pixels_height, int x1, int y1, int w, int h,
                       uint32_t color) {
-  for (int dy = 0; dy < (int)h; ++dy) {
-    int y = y0 + dy;
+  int x2 = x1 + OLIVEC_SIGN(int, w) * (OLIVEC_ABS(int, w) - 1);
+  if (x1 > x2)
+    OLIVEC_SWAP(int, x1, x2);
+  int y2 = y1 + OLIVEC_SIGN(int, h) * (OLIVEC_ABS(int, h) - 1);
+  if (y1 > y2)
+    OLIVEC_SWAP(int, y1, y2);
+
+  for (int y = y1; y <= y2; ++y) {
     if (0 <= y && y < (int)pixels_height) {
-      for (int dx = 0; dx < (int)w; ++dx) {
-        int x = x0 + dx;
+      for (int x = x1; x <= x2; ++x) {
         if (0 <= x && x < (int)pixels_width) {
           pixels[y * pixels_width + x] = color;
         }
@@ -99,40 +106,42 @@ void olivec_fill_circle(uint32_t *pixels, size_t pixels_width,
   }
 }
 
-void olivec_draw_line(uint32_t *pixels, size_t pixels_width, size_t pixels_height,
-                      int x1, int y1, int x2, int y2,
-                      uint32_t color)
-{
-    int dx = x2 - x1;
-    int dy = y2 - y1;
+void olivec_draw_line(uint32_t *pixels, size_t pixels_width,
+                      size_t pixels_height, int x1, int y1, int x2, int y2,
+                      uint32_t color) {
+  int dx = x2 - x1;
+  int dy = y2 - y1;
 
-    if (dx != 0) {
-        int c = y1 - dy*x1/dx;
+  if (dx != 0) {
+    int c = y1 - dy * x1 / dx;
 
-        if (x1 > x2) OLIVEC_SWAP(int, x1, x2);
-        for (int x = x1; x <= x2; ++x) {
-            if (0 <= x && x < (int) pixels_width) {
-                int sy1 = dy*x/dx + c;
-                int sy2 = dy*(x + 1)/dx + c;
-                if (sy1 > sy2) OLIVEC_SWAP(int, sy1, sy2);
-                for (int y = sy1; y <= sy2; ++y) {
-                    if (0 <= y && y < (int) pixels_height) {
-                        pixels[y*pixels_width + x] = color;
-                    }
-                }
-            }
+    if (x1 > x2)
+      OLIVEC_SWAP(int, x1, x2);
+    for (int x = x1; x <= x2; ++x) {
+      if (0 <= x && x < (int)pixels_width) {
+        int sy1 = dy * x / dx + c;
+        int sy2 = dy * (x + 1) / dx + c;
+        if (sy1 > sy2)
+          OLIVEC_SWAP(int, sy1, sy2);
+        for (int y = sy1; y <= sy2; ++y) {
+          if (0 <= y && y < (int)pixels_height) {
+            pixels[y * pixels_width + x] = color;
+          }
         }
-    } else {
-        int x = x1;
-        if (0 <= x && x < (int) pixels_width) {
-            if (y1 > y2) OLIVEC_SWAP(int, y1, y2);
-            for (int y = y1; y <= y2; ++y) {
-                if (0 <= y && y < (int) pixels_height) {
-                    pixels[y*pixels_width + x] = color;
-                }
-            }
-        }
+      }
     }
+  } else {
+    int x = x1;
+    if (0 <= x && x < (int)pixels_width) {
+      if (y1 > y2)
+        OLIVEC_SWAP(int, y1, y2);
+      for (int y = y1; y <= y2; ++y) {
+        if (0 <= y && y < (int)pixels_height) {
+          pixels[y * pixels_width + x] = color;
+        }
+      }
+    }
+  }
 }
 
 #endif // OLIVE_C_
