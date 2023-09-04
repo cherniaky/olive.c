91212c4947d30a78fa8c2e35090fbf0072bdcef0
CherniakYura <juracherniak@gmail.com>
2023-09-05 00:01:50 +0300

    Implement automatic testing

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 set -xe
 
 cc -Wall -Wextra -ggdb -o ./bin/example example.c
+cc -Wall -Wextra -ggdb -o ./bin/test test.c -lm
--- olive.c
+++ olive.c
@@ -8,12 +8,6 @@
 
 typedef int Errno;
 
-#define return_defer(value)                                                    \
-  do {                                                                         \
-    result = (value);                                                          \
-    goto defer;                                                                \
-  } while (0)
-
 #define OLIVEC_SWAP(T, a, b)                                                   \
   do {                                                                         \
     T t = a;                                                                   \
@@ -28,39 +22,6 @@ void olivec_fill(uint32_t *pixels, size_t width, size_t height,
   for (size_t i = 0; i < width * height; ++i) {
     pixels[i] = color;
   }
-}
-
-Errno olivec_save_to_ppm_file(uint32_t *pixels, size_t width, size_t height,
-                              const char *file_path) {
-  int result = 0;
-  FILE *f = NULL;
-
-  {
-    f = fopen(file_path, "wb");
-    if (f == NULL)
-      return_defer(errno);
-
-    fprintf(f, "P6\n%zu %zu 255\n", width, height);
-    if (ferror(f))
-      return_defer(errno);
-
-    for (size_t i = 0; i < width * height; ++i) {
-      uint32_t pixel = pixels[i];
-      uint8_t bytes[3] = {
-          (pixel >> (8 * 0)) & 0xFF,
-          (pixel >> (8 * 1)) & 0xFF,
-          (pixel >> (8 * 2)) & 0xFF,
-      };
-      fwrite(bytes, sizeof(bytes), 1, f);
-      if (ferror(f))
-        return_defer(errno);
-    }
-  }
-
-defer:
-  if (f)
-    fclose(f);
-  return result;
 }
 
 void olivec_fill_rect(uint32_t *pixels, size_t pixels_width,
