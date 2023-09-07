b0ef65a653fc05fef501483f729fd1314329d9ac
CherniakYura <juracherniak@gmail.com>
2023-09-07 10:17:27 +0300

    Add support for SDL2

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ cc -Wall -Wextra -ggdb -o ./bin/test -Ithirdparty test.c -lm
 cc -Wall -Wextra -ggdb -o ./bin/gallery -Ithirdparty -I. examples/gallery.c
 clang -Os -fno-builtin -Wall -Wextra -Wswitch-enum --target=wasm32 --no-standard-libraries -Wl,--no-entry -Wl,--export-all -Wl,--allow-undefined  -o ./bin/triangle.wasm -I. ./examples/triangle.c
 ./bin/test
+cc -Wall -Wextra -ggdb -I. -DSDL_PLATFORM -o ./bin/triangle ./examples/triangle.c -lm -lSDL2
--- examples/triangle.c
+++ examples/triangle.c
@@ -3,8 +3,8 @@
 #define WIDTH 800
 #define HEIGHT 600
 
-uint32_t pixels[WIDTH*HEIGHT];
-float angle = 0;
+static uint32_t pixels[WIDTH * HEIGHT];
+static float angle = 0;
 
 float sqrtf(float x);
 float atan2f(float y, float x);
@@ -13,29 +13,100 @@ float cosf(float x);
 
 #define PI 3.14159265359
 
-void rotate_point(int *x, int *y)
-{
-    int dx = *x - WIDTH/2;
-    int dy = *y - HEIGHT/2;
-    float mag = sqrtf(dx*dx + dy*dy);
-    float dir = atan2f(dy, dx) + angle;
-    *x = cosf(dir)*mag + WIDTH/2; 
-    *y = sinf(dir)*mag + HEIGHT/2;
+void rotate_point(int *x, int *y) {
+  int dx = *x - WIDTH / 2;
+  int dy = *y - HEIGHT / 2;
+  float mag = sqrtf(dx * dx + dy * dy);
+  float dir = atan2f(dy, dx) + angle;
+  *x = cosf(dir) * mag + WIDTH / 2;
+  *y = sinf(dir) * mag + HEIGHT / 2;
 }
 
-uint32_t *render(float dt)
-{
-    angle += 0.5f*PI*dt;
+uint32_t *render(float dt) {
+  angle += 0.5f * PI * dt;
 
-    olivec_fill(pixels, WIDTH, HEIGHT, 0xFF181818);
+  olivec_fill(pixels, WIDTH, HEIGHT, 0xFF181818);
+  {
+    int x1 = WIDTH / 2, y1 = HEIGHT / 8;
+    int x2 = WIDTH / 8, y2 = HEIGHT / 2;
+    int x3 = WIDTH * 7 / 8, y3 = HEIGHT * 7 / 8;
+    rotate_point(&x1, &y1);
+    rotate_point(&x2, &y2);
+    rotate_point(&x3, &y3);
+    olivec_fill_triangle(pixels, WIDTH, HEIGHT, x1, y1, x2, y2, x3, y3,
+                         0xFF2020AA);
+  }
+  return pixels;
+}
+
+#ifdef SDL_PLATFORM
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#define return_defer(value) do { result = (value); goto defer; } while (0)
+
+int main(void)
+{
+    int result = 0;
+
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Texture *texture = NULL;
+
     {
-        int x1 = WIDTH/2, y1 = HEIGHT/8;
-        int x2 = WIDTH/8, y2 = HEIGHT/2;
-        int x3 = WIDTH*7/8, y3 = HEIGHT*7/8;
-        rotate_point(&x1, &y1);
-        rotate_point(&x2, &y2);
-        rotate_point(&x3, &y3);
-        olivec_fill_triangle(pixels, WIDTH, HEIGHT, x1, y1, x2, y2, x3, y3, 0xFF2020AA);
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) return_defer(1);
+
+        window = SDL_CreateWindow("Olivec", 0, 0, WIDTH, HEIGHT, 0);
+        if (window == NULL) return_defer(1);
+
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (renderer == NULL) return_defer(1);
+
+        texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
+        if (texture == NULL) return_defer(1);
+
+        Uint32 prev = SDL_GetTicks();
+        for (;;) {
+            // Compute Delta Time
+            Uint32 curr = SDL_GetTicks();
+            float dt = (curr - prev)/1000.f;
+            prev = curr;
+
+            // Flush the events
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) if (event.type == SDL_QUIT) return_defer(0);
+
+            // Render the texture
+            SDL_Rect window_rect = {0, 0, WIDTH, HEIGHT};
+            uint32_t *pixels_src = render(dt);
+            void *pixels_dst;
+            int pitch;
+            if (SDL_LockTexture(texture, &window_rect, &pixels_dst, &pitch) < 0) return_defer(1);
+            for (size_t y = 0; y < HEIGHT; ++y) {
+                memcpy(pixels_dst + y*pitch, pixels_src + y*WIDTH, WIDTH*sizeof(uint32_t));
+            }
+            SDL_UnlockTexture(texture);
+
+            // Display the texture
+            if (SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0) < 0) return_defer(1);
+            if (SDL_RenderClear(renderer) < 0) return_defer(1);
+            if (SDL_RenderCopy(renderer, texture, &window_rect, &window_rect) < 0) return_defer(1);
+            SDL_RenderPresent(renderer);
+        }
     }
-    return pixels;
+
+defer:
+    if (texture) SDL_DestroyTexture(texture);
+    if (renderer) SDL_DestroyRenderer(renderer);
+    if (window) SDL_DestroyWindow(window);
+    switch (result) {
+    case 0:
+        printf("OK\n");
+        break;
+    default:
+        fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
+    }
+    SDL_Quit();
+    return result;
 }
+#endif // SDL_PLATFORM
