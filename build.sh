5a9a17e1eaf74d3af1811dd30698686e06176743
CherniakYura <juracherniak@gmail.com>
2023-09-12 23:59:31 +0300

    Prepare for GitHub pages deployment

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ build_vc_example() {
     NAME=$1
 
     clang -Os -fno-builtin -Wall -Wextra -Wswitch-enum --target=wasm32 --no-standard-libraries -Wl,--no-entry -Wl,--export=render -Wl,--export=init -Wl,--allow-undefined -I. -I./build/ -I./thirdparty/ -o ./build/$NAME.wasm -DPLATFORM=WASM_PLATFORM ./examples/$NAME.c
+    mkdir -p ./wasm/
+    cp ./build/$NAME.wasm ./wasm/
+
     clang -Wall -Wextra -ggdb -I. -I./build/ -I./thirdparty/ -o ./build/$NAME.term -DPLATFORM=TERM_PLATFORM ./examples/$NAME.c -lm
 }
 
