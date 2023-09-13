b6d568f394e26d18deceb51a2d89baffa0423adc
CherniakYura <juracherniak@gmail.com>
2023-09-13 16:55:50 +0300

    Remove deadcode from test.c

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 set -xe
 
-build_vc_example() {
+build_vc_demo() {
     NAME=$1
 
-    clang -Os -fno-builtin -Wall -Wextra -Wswitch-enum --target=wasm32 --no-standard-libraries -Wl,--no-entry -Wl,--export=render -Wl,--export=init -Wl,--allow-undefined -I. -I./build/ -I./thirdparty/ -o ./build/$NAME.wasm -DPLATFORM=WASM_PLATFORM ./examples/$NAME.c
+    clang -Os -fno-builtin -Wall -Wextra -Wswitch-enum --target=wasm32 --no-standard-libraries -Wl,--no-entry -Wl,--export=render -Wl,--export=init -Wl,--allow-undefined -I. -I./build/ -I./thirdparty/ -o ./build/$NAME.wasm -DPLATFORM=WASM_PLATFORM ./demos/$NAME.c
     mkdir -p ./wasm/
     cp ./build/$NAME.wasm ./wasm/
 
-    clang -Wall -Wextra -ggdb -I. -I./build/ -I./thirdparty/ -o ./build/$NAME.term -DPLATFORM=TERM_PLATFORM ./examples/$NAME.c -lm
+    clang -Wall -Wextra -ggdb -I. -I./build/ -I./thirdparty/ -o ./build/$NAME.term -DPLATFORM=TERM_PLATFORM ./demos/$NAME.c -lm
 }
 
 mkdir -p ./build/
@@ -20,7 +20,7 @@ clang -Wall -Wextra -ggdb -o ./build/png2c -Ithirdparty png2c.c -lm
 mkdir -p ./build/assets/
 ./build/png2c ./assets/nikita.png > ./build/assets/nikita.c
 
-build_vc_example triangle &
-build_vc_example 3d &
-build_vc_example squish &
+build_vc_demo triangle &
+build_vc_demo 3d &
+build_vc_demo squish &
 wait
